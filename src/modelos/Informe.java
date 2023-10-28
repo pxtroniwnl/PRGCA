d6b72e96f399ec02a695726fef293513d1c7ae4d
Alejandro Patron Montero <alejandropatronm@gmail.com>
2023-10-28 09:07:27 -0500

    + Clase Modelo de Informes (Le falta la clase ubicacion 	como atributo)

---
+++ src/modelos/Informe.java
@@ -0,0 +1,11 @@
+
+package modelos;
+
+public class Informe {
+    private int IdInforme;
+    //ATRIBUTOS DE CLASE UBICACION (FALTA CREAR CLASE UBICACION QUE ESTARA CONFORMADA POR: CLASE LOCALIDAD, UNIDAD COMUNERA, BARRIO)
+    private Contaminante contaminante;
+    private EstadoRevision estado; //Por defecto constructor = new (1, "Sin atender")
+    
+    
+}
