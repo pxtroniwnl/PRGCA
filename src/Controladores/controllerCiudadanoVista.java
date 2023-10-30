ab06fce07b55034557fdc4588103071374ffbad1
Alejandro Patron Montero <alejandropatronm@gmail.com>
2023-10-30 11:16:34 -0500

    Progreso 0.4¿5
    +Creacion de clases controladoras (Sin metodos ni atributos dentro)
    
    -Falta clases modelo y ocntroladores

--- src/Controladores/controllerCiudadanoVista.java
+++ src/Controladores/controllerCiudadanoVista.java
@@ -1,21 +1,24 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-public class controllerAdminVista implements Initializable{
+/**
+ *
+ * @author ALEJANDRO PATRON
+ */
+public class controllerCiudadanoVista implements Initializable{
     
     
-    //DECLARAMOS LO QUE TENGAMOS EN LA INTERFAZ
-    
-    //METODO PARA EL CLICK DE LOS BOTONES DE LA INTERFAZ ADMIN
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
     
 }
--- src/Controladores/controllerEntidadVista.java
+++ src/Controladores/controllerEntidadVista.java
@@ -1,21 +1,22 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-public class controllerAdminVista implements Initializable{
-    
-    
-    //DECLARAMOS LO QUE TENGAMOS EN LA INTERFAZ
-    
-    //METODO PARA EL CLICK DE LOS BOTONES DE LA INTERFAZ ADMIN
-    
+/**
+ *
+ * @author ALEJANDRO PATRON
+ */
+public class controllerEntidadVista implements Initializable{
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
     
 }
