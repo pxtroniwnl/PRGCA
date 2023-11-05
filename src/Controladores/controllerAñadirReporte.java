16bf47621cf33acb58060d394790a4ae88be995a
Alejandro Patron Montero <alejandropatronm@gmail.com>
2023-11-05 01:18:10 -0500

    Progreso 2.8
    + ControllerAñadirReporte Terminado falta ver las metricas
    
    -Falta clases modelo y controladores (AdminVista, CiudadanoVista, EntidadVista)

--- src/Controladores/controllerAñadirReporte.java
+++ src/Controladores/controllerAñadirReporte.java
@@ -8,7 +8,6 @@ import ConexionBaseDeDatos.ConexionBDD;
 import Modelos.Barrio;
 import Modelos.Contaminante;
 import Modelos.Localidad;
-import Modelos.Rol;
 import Modelos.UnidadComunera;
 import java.net.URL;
 import java.sql.Connection;
@@ -22,11 +21,11 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -73,19 +72,41 @@ public class controllerAñadirReporte implements Initializable {
         if (IDUsuarioTextField.getText().isBlank() && DescripcionTextField.getText().isBlank()) {
             System.out.println("Por favor, complete todos los campos.");
         } else {
-            System.out.println("Ningun espacio en blanco");
-            //Llamar al metodo que Inserta los datos
+            Contaminante selectedContam = ContaminanteComboBox.getValue();
+            Localidad selectedLocalidad = LocalidadComboBox.getValue();
+            UnidadComunera selectedUnidad = UnidadComuneraComboBox.getValue();
+            Barrio selectedBarrio = BarrioComboBox.getValue();
+            
+            if(selectedContam == null || selectedLocalidad == null || selectedUnidad == null || selectedBarrio == null){
+                JOptionPane.showMessageDialog(null, "Eliga un campo");
+            }else{
+                //METODO PARA AÑADIR REPORTES
+                añadirReporte();
+            }
         }
     }
-
+    
+    //METODO PARA CARGAR LOS CONTAMINANTES
+    private void cargarContaminante(){
+        Contaminante c1 = new Contaminante(1, "Contaminacion del Aire");
+        Contaminante c2 = new Contaminante(2, "Contaminacion del Agua");
+        Contaminante c3 = new Contaminante(3, "Contaminacion del Suelo");
+        Contaminante c4 = new Contaminante(4, "Contaminacion Acustica");
+        Contaminante c5 = new Contaminante(5, "Contaminacion Luminica");
+        Contaminante c6 = new Contaminante(6, "Contaminacion Visual");
+        Contaminante c7 = new Contaminante(7, "Contaminacion Termica");
+        Contaminante c8 = new Contaminante(8, "Contaminacion de Fauna");
+        
+        ContaminanteComboBox.getItems().addAll(c1,c2,c3,c4,c5,c6,c7,c8);
+    }
+    
     //METODO PARA CARGAR LOCALIDADES
-    private void cargarLocalidades() throws ClassNotFoundException, SQLException {
+    private void cargarLocalidades(){
         Localidad loc1 = new Localidad(1,"Historica y del Caribe Norte");
         Localidad loc2 = new Localidad(2,"De la Virgen y Turistica");
         Localidad loc3 = new Localidad(3,"Industrial y de la Bahia");
 
         LocalidadComboBox.getItems().addAll(loc1, loc2, loc3);
-
     }
 
     //METODO PARA CARGAR UNIDADES COMUNERAS
@@ -108,6 +129,7 @@ public class controllerAñadirReporte implements Initializable {
             UnidadComunera unidad = new UnidadComunera(id, nombre);
             unidadesList.add(unidad);
         }
+        UnidadComuneraComboBox.getItems().addAll(unidadesList);
     }
 
     //METODO PARA CARGAR BARRIOS
@@ -138,7 +160,6 @@ public class controllerAñadirReporte implements Initializable {
         Localidad selectedLocalidad = LocalidadComboBox.getValue();
         if (selectedLocalidad != null) {
             cargarUnidadesComuneras(selectedLocalidad.getNumeroLocalidad());
-
         }
     }
 
@@ -150,30 +171,48 @@ public class controllerAñadirReporte implements Initializable {
             cargarBarrios(selectedUnidadComunera.getIdUnidad());
         }
     }
+    
+    //METODO DEL INSERT A LA BASE DE DATOS
+    private void añadirReporte() throws ClassNotFoundException, SQLException{
+        ConexionBDD conexionActual = new ConexionBDD(); //Creamos una nueva conexion de la clase ConexionBDD (Objeto de esta clase con el constructor por defecto)
+        Connection conectarBDD = (Connection) conexionActual.getConnection(); //Conectamos e inicializamos la BDD haciendo uso de un casteo
+        
+        String selectedContam = ContaminanteComboBox.getValue().getNombreContam(); //Obtiene el tipo de contaminacion elegido
+        String selectedLocalidad = LocalidadComboBox.getValue().getNombreLocalidad(); //Obtiene la localidad elegida
+        String selectedUnidad = UnidadComuneraComboBox.getValue().getNombreUnidad(); //Obtiene la unidad elegida
+        String selectedBarrio = BarrioComboBox.getValue().getNombreBarrio(); //Obtiene el barrio elegio
+        
+        // Definimos la 'consulta' o 'query' para registrar nuevos registros en los respectivos campos de valor
+        String consultaQuery = "INSERT INTO reportes (idUsuario, contaminante, localidad, unidad, barrio, Descripcion) "
+            + "VALUES ('" + IDUsuarioTextField.getText() + "','" + selectedContam + "','" + selectedLocalidad 
+            + "','" + selectedUnidad + "','" + selectedBarrio + "','" + DescripcionTextField.getText() + "')";
+        
+        //Instanciamos de tipo statement para la consola
+        Statement instancia = (Statement) conectarBDD.createStatement();
 
+        int rQ = instancia.executeUpdate(consultaQuery);
+
+        //CONDICIONAL PARA SABER SI SE ENCONTRO EL USUARIO O NO (DEPENDIENDO DE LAS FILAS)
+        if (rQ > 0) {
+            // Si se encontró un usuario, la consulta devolverá al menos una fila
+            JOptionPane.showMessageDialog(null, "¡Reporte Registrado!");
+            Stage stage = (Stage) registrarBttn.getScene().getWindow();
+            stage.close();
+        } else {
+            // Si no se encontró un usuario, la consulta no devolverá filas
+            JOptionPane.showMessageDialog(null, "Usuario no registrado. Verifica tus credenciales.");
+        }   
+    }
+
+    /**
+     *
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        try {
-            cargarLocalidades();
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(controllerAñadirReporte.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        LocalidadComboBox.setOnAction(event -> {
-            try {
-                this.localidadComboBoxOnAction(event);
-            } catch (SQLException | ClassNotFoundException ex) {
-                Logger.getLogger(controllerAñadirReporte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        });
-        
-        UnidadComuneraComboBox.setOnAction(event -> {
-            try {
-                this.unidadComuneraComboBoxOnAction(event);
-            } catch (SQLException | ClassNotFoundException ex) {
-                Logger.getLogger(controllerAñadirReporte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        });
-
+        cargarLocalidades();
+        cargarContaminante();
     }
+    
 }
